beb7b37be6d0fa39d4b2b2508c0f941d6c9b32eb
Lucas André <lucasandrelescanocruz@gmail.com>
2019-01-09 09:04:19 -0300

    Migrando estudante e update do banco

---
+++ WebAppAula02CRUD/Migrations/20190109114404_Inicio.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppAula02CRUD.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudante",
+                columns: table => new
+                {
+                    EstudanteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CPF = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudante", x => x.EstudanteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudante");
+        }
+    }
+}
